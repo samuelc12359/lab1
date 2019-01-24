a1ffb2a1bd5eb49d8f7430ca3d2fffcf9ec59844
Sameul Cole <scole1@iastate.edu>
2019-01-24 13:48:02 -0600

    lab 1.

---
+++ Lab1.R
@@ -0,0 +1,40 @@
+
+3
+pi
+Tru
+FALSE
+class(1)
+class(TRUE)
+
+
+
+seq(1,10)
+seq(0,10, by=2)
+?seq
+c((seq(1,10, by=2)), (10:1))
+
+numGender <- c(25,15)
+names(numGender) <- c('male','female')
+numGender
+str(numGender)
+
+str(c)
+str(numGender)
+a=1
+a = 1:5
+b = 5:1
+a + b
+a + 1
+a^2
+
+
+
+x <- c(4,1,3,9)
+x
+y <- c(1,2,3,5)
+y
+
+d = sqrt(sum(x-y)^2)
+d
+git remote add origin https://github.com/samuelc12359/lab1.git
+git push -u origin master
